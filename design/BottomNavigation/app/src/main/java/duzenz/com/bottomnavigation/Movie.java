333ce3e1722d0ff516f31b7e82ddfb2e0dd85b9a
duzenz <duzenz@gmail.com>
2018-01-11 23:21:15 +0300

    bottom navigation example

---
+++ design/BottomNavigation/app/src/main/java/duzenz/com/bottomnavigation/Movie.java
@@ -0,0 +1,36 @@
+package duzenz.com.bottomnavigation;
+
+/**
+ * Created by z00284305 on 11-Jan-18.
+ */
+
+public class Movie {
+
+    String title;
+    String image;
+    String price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
