5a979293e588ffdbd16100d37725f9a28ac956b2
duzenz <duzenz@gmail.com>
2018-01-14 14:29:09 +0300

    activity recognition

---
+++ feature/ActivityRecognition/app/src/main/java/duzenz/com/myapplication/Constants.java
@@ -0,0 +1,14 @@
+package duzenz.com.myapplication;
+
+/**
+ * Created by z00284305 on 14-Jan-18.
+ */
+
+public class Constants {
+
+    public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
+
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+
+    public static final int CONFIDENCE = 70;
+}
